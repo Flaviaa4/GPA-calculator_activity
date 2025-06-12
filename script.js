87c8a900e8ea53b83feebc6b89e08b02840d096c
Flaviaa4 <f.benihirwe@alustudent.com>
2025-06-12 04:16:40 -0700

    file of GPA Calculator

---
+++ script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const name = document.getElementById("name").value;
+const assignment = document.getElementById("assig").value;
+const grade = document.getElementById("grade").value;
+const button = document.getElementById("btn")
